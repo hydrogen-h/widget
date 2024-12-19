a129bc3c0df40d3dbfe212b8910929d83c9aa141
hydrogen-h <hydrogen_h714@163.com>
2024-12-19 18:14:17 +0800

    postMessage通信 widgetId

--- src/main.js
+++ src/main.js
@@ -1,17 +1,34 @@
 class Widget {
-    constructor() {
-    }
+  constructor() {}
 
-    async initialize({ targetId }) {
-        const t = document.getElementById(targetId)
-        const iframe = document.createElement('iframe');
-        iframe.src = 'https://staging-app.avitor.ai/me/request-quote-widget'
-        iframe.width = '100%';
-        iframe.height = '100%';
-        iframe.style.border = 'none'
-        iframe.style.minHeight = '40rem'
-        t.appendChild(iframe);
+  async initialize({ targetId, widgetId }) {
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      const iframe = document.createElement("iframe");
+      iframe.src = 'https://staging-app.avitor.ai/request-quote-widget';
+      iframe.width = "100%";
+      iframe.height = "100%";
+      iframe.style.border = "none";
+      iframe.style.minHeight = "40rem";
+      iframe.referrerPolicy = "origin";
+      targetElement.appendChild(iframe);
+
+      if (iframe.contentWindow) {
+        iframe.contentWindow.postMessage(
+          {
+            type: "widget-sign",
+            data: {
+              widgetId: widgetId,
+            },
+          },
+          "*"
+        );
+      }
+    } else {
+      console.error("Target element not found.");
     }
+  }
 }
 
 export default new Widget();
