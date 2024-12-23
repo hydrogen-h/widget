179a3b32938ecab4138947a4b3ffd6917b4bc6f3
wuyan <hydrogen_h714@163.com>
2024-12-24 00:08:36 +0800

    为每个widget安置权限

--- src/main.js
+++ src/main.js
@@ -1,30 +1,18 @@
 class Widget {
   constructor() {}
 
-  async initialize({ targetId, widgetId }) {
+  async initialize({ targetId, widgetId, salt }) {
     const targetElement = document.getElementById(targetId);
 
     if (targetElement) {
       const iframe = document.createElement("iframe");
-      iframe.src = 'https://staging-app.avitor.ai/request-quote-widget';
+      iframe.src = `https://staging-app.avitor.ai/request-quote-widget?widgetId=${widgetId}&salt=${salt}`;
       iframe.width = "100%";
       iframe.height = "100%";
       iframe.style.border = "none";
       iframe.style.minHeight = "40rem";
       iframe.referrerPolicy = "origin";
       targetElement.appendChild(iframe);
-
-      if (iframe.contentWindow) {
-        iframe.contentWindow.postMessage(
-          {
-            type: "widget-sign",
-            data: {
-              widgetId: widgetId,
-            },
-          },
-          "*"
-        );
-      }
     } else {
       console.error("Target element not found.");
     }
